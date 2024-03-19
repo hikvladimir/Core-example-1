de9df23880fd5f8489f2323eafdc3f7f0a04ae88
hikvladimir <psihouse@mail.ru>
2024-03-19 22:03:57 +0300

    ready

--- src/task/methods/Methods.java
+++ src/task/methods/Methods.java
@@ -4,9 +4,23 @@ public class Methods {
     public static void main(String[] args) {
         // Пример вызова метода
         String string = getString("HELLO");
-       System.out.println(string);
+        System.out.println(string);
         // Вызовы ваших методов
-        System.out.println(sum(1,2));
+        System.out.println(sum(1, 2));
+        System.out.println(sumStr("Hello ", "world!"));
+        System.out.println(sumStrInt("14", 4));
+        System.out.println(isStringLess("2", 4));
+        System.out.println(isBoolean(false, false));
+        System.out.println(addInt(2));
+       // int[] arr={1,2,3,4};
+
+        System.out.println(evenNumbers(new int[]{1, 2, 3, 4}));
+        System.out.println(newArray(new int[]{1, 2, 3, 4}));
+        System.out.println(summArr(new int[]{1, 2, 3, 4}));
+        getAge(22);
+        System.out.println(comparisonName("sdf","sdf"));
+        System.out.println(getLastIntArray(new int[]{4,3,2,6}));
+        System.out.println(counterCharA("sAAd"));
     }
 
     // Метод принимающий строку и преобразующий ее в нижний регистр
@@ -16,52 +30,126 @@ public class Methods {
 
 // Реализуйте функциональность согласно описанию
 
-// TODO: Создайте метод который вернет сумму двух чисел
-    public static int sum(int a, int b) { return a + b;}
+    // TODO: Создайте метод который вернет сумму двух чисел
+    public static int sum(int a, int b) {
+        return a + b;
+    }
 
 
-// TODO: Создайте метод который вернет сумму двух переданных строк
+    // TODO: Создайте метод который вернет сумму двух переданных строк
+    public static String sumStr(String str1, String str2) {
+        return str1 + str2;
+    }
 
 
-// TODO: создайте метод который примет строку внутри которой передается число , а также принимает число . Метод должен суммировать числа
+    // TODO: создайте метод который примет строку внутри которой передается число , а также принимает число . Метод должен суммировать числа
 //  обернуть их в строку и вернуть результат строкой
+    public static String sumStrInt(String str, int a) {
+        return String.valueOf(Integer.parseInt(str) + a);
+    }
 
-
-// TODO: Создайте метод который принимает строку , число . Внутри строки передается число . Проверить , что если число внутри строки больше чем
+    // TODO: Создайте метод который принимает строку , число . Внутри строки передается число . Проверить , что если число внутри строки больше чем
 //  число которое передается как int , то пусть метод вернет false , а если меньше , то true
+    public static boolean isStringLess(String str, int a) {
+        return Integer.parseInt(str) <= a;
+    }
 
 
-// TODO: Создайте метод который принимает 2 булевые переменные . Если обе true , верни true , если хоть одна из них true верни true ,
+    // TODO: Создайте метод который принимает 2 булевые переменные . Если обе true , верни true , если хоть одна из них true верни true ,
 //  если обе false верни false
+    public static boolean isBoolean(boolean a, boolean b) {
+//        if (a || b) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+        return a||b;
+    }
 
 
-// TODO: Создайте метод который принимает число и вернет true если оно четное , и false если оно не четное
+    // TODO: Создайте метод который принимает число и вернет true если оно четное , и false если оно не четное
+    public static boolean addInt(int value) {
+        return value % 2 == 0;
+    }
 
 
 // TODO: Создайте метод который проверят числа в массиве , если все они четные верни "Массив четных чисел" , если есть и четные и не
 //  четные верни "Массив четных и не четных чисел" и если в массиве только нечетные числа верни "Массив не четных чисел"
+public static String evenNumbers(int[] array) {
+    int even = 0;
+    for (int j : array) {
+        if (j % 2 == 0) {
+            even++;
+        }
+    }
+    return (even == array.length) ? "Массив четных " : (even == 0) ? "Массив нечетных " : "Массив четных и не четных";
+}
 
 
 // TODO: Создайте метод который принимает массив чисел и возвращает новый массив чисел . Внутри метода пройдитесь по элементам
 //  массива который подается в аргументы  и сложите все элементы в новый массив и верните его
+    public static int[] newArray(int[] array) {
+        int[] arrey1 = new int[array.length];
+        System.arraycopy(array, 0, arrey1, 0, array.length);
+        return arrey1;
+    }
 
 
 // TODO: Создайте метод который принимает массив чисел и возвращает число . Внутри метода пройдитесь по элементам массива который подается в
 //  аргументы сложите все числа который переданы в массиве и верните сумму всех чисел
+    public static  int summArr(int[] array) {
+        int summ=0;
+        for (int j : array) {
+            summ += j;
+        }
+        return summ;
+    }
 
 
 // TODO: Создайте методы в которые можно будет передавать свой возраст , а возвращаться сколько полных месяцев в этом возрасте ,
 //  тоесть если человек прожил 10 лет , это 120 месяцевтак же сделайте перевод возраста в минуты и метод который расчетает возраст в часах
-
+    public static void getAge(int age) {
+    int mount = age * 12;
+    int hour = (mount * 30) * 24;
+    int minute = hour * 60;
+    System.out.println("Человек прожил" + " месяцев:" + mount + " часов:" + hour + " минут:" + minute);
+}
 
 // TODO: Создайте метод которому передаю имена , а он проверяет одинаковые ли они , причем не зависимо от регистра
+    public static boolean comparisonName(String name, String name2) {
+    return name.equalsIgnoreCase(name2);
+}
 
 
 // TODO: Создайте метод которому можно передать массив элементов , а он вернет последний элемент
-
+    public static int getLastIntArray(int[] array) {
+        return array[array.length-1];
+    }
 
 // TODO: Создайте метод счетчик букв А . Подается строка и метод посчитает сколько букв А в строке
-
+    public static int counterCharA(String string) {
+        int result = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == 'A') {
+                result++;
+            }
+        }
+        return result;
+    }
 
 // TODO: Создайте метод кошелек , у него есть началная сумма и можно вычитать пока деньги не кончатся или сумма не будет слишком велика
+
+    private static int wallet = 550;
+
+    public static void wallet(char character, int money) {
+        if (character == '-') {
+            wallet -= money;
+        } else if (character == '+')
+            wallet += money;
+        if (wallet < 0 || wallet > 1000) {
+            System.out.println("Eror");
+        } else {
+            System.out.println(wallet);
+        }
+    }
 }
--- src/task/oop/OOP.java
+++ src/task/oop/OOP.java
@@ -14,6 +14,31 @@ public class OOP {
     //  4. Метод, не принимающий и не возвращающий ничего, лишь выводящий на экран "I am Parent".
     //  5. Метод, возвращающий конкатенацию (сумму двух строк), принимающий 2 строки.
 
+    class Parent {
+        public double sum(double value1, double value2) {
+            return value1 + value2;
+        }
+
+        public int sumInt(String st) {
+            String value = String.valueOf(st.charAt(0));
+            String value2 = String.valueOf(st.charAt(1));
+            int result = Integer.parseInt(value);
+            int result2 = Integer.parseInt(value2);
+            return result + result2;
+        }
+
+        public String string(String st) {
+            return st;
+        }
+
+        Parent() {
+            System.out.println("I am Parent");
+        }
+
+        public String subString(String st, String st2) {
+            return st + st2;
+        }
+    }
 
     // TODO: Создайте класс Child1 и переопределите все 5 методов.
     //  1. Первый метод переопределите так, что вместо суммы первый метод перемножает числа, а не складывает.
@@ -21,6 +46,35 @@ public class OOP {
     //  3. Третий метод переопределите, чтобы возвращал только первую букву из строки.
     //  4. Четвертый метод выводит на экран "I am Child1".
     //  5. Пятый метод после слияния двух строк должен обрезать строку и вернуть только ее половину.
+
+    class Child extends Parent {
+        @Override
+        public double sum(double value1, double value2) {
+            return value1 * value2;
+        }
+
+        @Override
+        public int sumInt(String st) {
+            String value = String.valueOf(st.charAt(0));
+            String value2 = String.valueOf(st.charAt(1));
+            return Integer.parseInt(value) * Integer.parseInt(value2);
+        }
+
+        @Override
+        public String string(String st) {
+            return String.valueOf(st.charAt(0));
+        }
+
+        Child() {
+            System.out.println("I am Child");
+        }
+
+        @Override
+        public String subString(String st, String st2) {
+            int central = (st + st2).length() / 2;
+            return (st + st2).substring(0, central);
+        }
+    }
 
 
     // TODO: Создайте класс Child2 и перегрузите методы.
@@ -30,14 +84,156 @@ public class OOP {
     //  4. Четвертый метод примет аргумент строку и выведет на экран "I am Child2" + переданная строка.
     //  5. Пятый метод принимает 3 строки и возвращает их конкатенацию.
 
+    class Child2 extends Parent {
+        public double Sum(double value1, double value2, double value3) {
+            return value1 + value2 + value3;
+        }
+
+        public int SumInt(String st, String st2, String st3) {
+            String value = String.valueOf(st.charAt(0));
+            String value2 = String.valueOf(st2.charAt(0));
+            String value3 = String.valueOf(st3.charAt(0));
+            return Integer.parseInt(value) + Integer.parseInt(value2) + Integer.parseInt(value3);
+        }
+
+        public String string(String st, String st2) {
+            return st + st2;
+        }
+
+        Child2(String st) {
+            System.out.println("I am Child2" + st);
+        }
+
+        public String subString(String st, String st2, String st3) {
+            return st + st2 + st3;
+        }
+    }
+
 
     // TODO: Создайте абстрактный класс "Фигура", в котором определены общие свойства и методы для всех геометрических фигур
     //  (например, метод для расчета площади).
     //  От него унаследуйте классы "Круг", "Прямоугольник" и "Треугольник", переопределив специфичные методы для каждой фигуры.
+
+    abstract class Figure {
+//        static final double pi = Math.PI;
+
+        Figure(String name) {
+            this.name = name;
+        }
+
+        String name;
+
+        abstract void square(double a, double b);
+    }
+
+    class Circle extends Figure {
+        Circle(String name) {
+            super(name);
+            this.name = name;
+        }
+
+        @Override
+        void square(double a, double b) {
+            System.out.println(Math.PI * a * b);
+        }
+    }
+
+    class Rectangle extends Figure {
+        Rectangle(String name) {
+            super(name);
+            this.name = name;
+        }
+
+        @Override
+        void square(double a, double b) {
+            System.out.println(a * b);
+        }
+    }
+
+    class Triangle extends Figure {
+        Triangle(String name) {
+            super(name);
+            this.name = name;
+        }
+
+        @Override
+        void square(double a, double b) {
+            System.out.println((a / 2) * b);
+        }
+    }
 
 
     // TODO: Создайте абстрактный класс "Сотрудник", в котором определены общие характеристики для всех сотрудников (например, имя, возраст).
     //  Создайте подклассы для различных типов сотрудников (например, "Менеджер", "Работник склада"),
     //  в которых можно описать специфичные методы и свойства.
 
+    abstract class Employee {
+        Employee(String name) {
+            this.name = name;
+        }
+
+        private int age;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        private String name;
+
+
+
+        public void setAge(int age) {
+            this.age = age;
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        abstract void work();
+
+        abstract void rest();
+    }
+
+    class Manager extends Employee {
+        Manager(String name) {
+            super();
+            getName(String name);
+        }
+
+        int age = 45;
+
+        @Override
+        void work() {
+            System.out.println("Управляет проектами");
+        }
+
+        @Override
+        void rest() {
+            System.out.println("Берет работу на дом");
+        }
+    }
+
+    class WarehouseWorker extends Employee {
+        WarehouseWorker(String name) {
+            super(name);
+        }
+
+        int age = 37;
+
+        @Override
+        void work() {
+            System.out.println("Заполняет накладную, и ведет учет");
+        }
+
+        @Override
+        void rest() {
+            System.out.println("Отдыхает после работы");
+        }
+    }
 }
